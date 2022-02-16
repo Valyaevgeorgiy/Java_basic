416f5781d04811d44ff597afb5224a3b55abd7ba
AsUsVeRsUs <71630161+Valyaevgeorgiy@users.noreply.github.com>
2022-02-16 14:54:49 +0300

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,16 +1,14 @@
-// file Hello.java
-
-int m, n = 2, 4
 
 public class Hello {
-    public static void main( String args[] )
-    {
-        System.out.println("Hello World!")
+    public static void main( String args[] ) {
+        
+        System.out.println("Hello World!") // вывод на экран
+            
         System.out.println("My arguments:")
-        for (int i = 0; i < args.length; i++)
-        {
+        for (int i = 0; i < args.length; i++) { // структура примитивного цикла for 
             System.out.println(" arg["+i"] :"+args[i]);
         }
+        
         System.out.println("Good bye!")
     }
 }
